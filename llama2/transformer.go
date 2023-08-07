b2eb09c31e1e3e0e31ae6d04212f49b01dfb51ac
Nikolay <nikolay.dubina.pub@gmail.com>
2023-08-07 12:36:50 +0700

    nits

--- llama2/transformer.go
+++ llama2/transformer.go
@@ -7,6 +7,40 @@ import (
 	nn "github.com/nikolaydubina/llama2.go/exp/nnfast"
 )
 
+type RunState struct {
+	// current wave of activations
+	X      []float32 // (dim,) activation at current time stamp
+	XB     []float32 // (dim,) same, but inside a residual branch
+	XB2    []float32 // (dim,) an additional buffer just for convenience
+	HB     []float32 // (hidden_dim,) buffer for hidden dimension in the FFN
+	HB2    []float32 // (hidden_dim,) buffer for hidden dimension in the FFN
+	Q      []float32 // (dim,) query
+	K      []float32 // (dim,) key
+	V      []float32 // (dim,) value
+	Att    []float32 // (n_heads, seq_len) buffer for scores/attention values
+	Logits []float32 // output logits
+	// kv cache
+	KeyCache []float32 // (layer, seq_len, dim)
+	ValCache []float32 // (layer, seq_len, dim)
+}
+
+func NewRunState(config Config) RunState {
+	return RunState{
+		X:        make([]float32, config.Dim),
+		XB:       make([]float32, config.Dim),
+		XB2:      make([]float32, config.Dim),
+		HB:       make([]float32, config.HiddenDim),
+		HB2:      make([]float32, config.HiddenDim),
+		Q:        make([]float32, config.Dim),
+		K:        make([]float32, config.Dim),
+		V:        make([]float32, config.Dim),
+		Att:      make([]float32, (config.NumHeads * config.SeqLen)),
+		Logits:   make([]float32, config.VocabSize),
+		KeyCache: make([]float32, (config.NumLayers * config.SeqLen * config.Dim)),
+		ValCache: make([]float32, (config.NumLayers * config.SeqLen * config.Dim)),
+	}
+}
+
 type TransformerWeights struct {
 	TokenEmbeddingTable []float32
 
@@ -79,12 +113,10 @@ func Transformer(token int, pos int, config Config, s RunState, w TransformerWei
 			}
 		}
 
-		// save key,value at this time step (pos) to our kv cache
+		// save key and val at this time step (pos) to cache
 		loff := l * config.SeqLen * dim
-		keyCacheRow := s.KeyCache[(loff + pos*dim):(loff + (pos+1)*dim)]
-		valCacheRow := s.ValCache[(loff + pos*dim):(loff + (pos+1)*dim)]
-		copy(keyCacheRow, s.K)
-		copy(valCacheRow, s.V)
+		copy(s.KeyCache[(loff+pos*dim):(loff+(pos+1)*dim)], s.K) // key cache row
+		copy(s.ValCache[(loff+pos*dim):(loff+(pos+1)*dim)], s.V) // val cache row
 
 		// multithread attention. iterate over all heads
 		// C code had pragma here, using goroutines
@@ -111,7 +143,7 @@ func Transformer(token int, pos int, config Config, s RunState, w TransformerWei
 					att[t] = score
 				}
 
-				// softmax the scores to get attention weights, from 0..pos inclusively
+				// scores to get attention weights, from 0..pos inclusively
 				nn.SoftMax(att[:pos+1])
 
 				// weighted sum of the values, store back into xb
